f393fc04a4ad5b53a0063d4651f64b0cc87d5cf7
Felipe Couto <fel.h2o@gmail.com>
2017-02-24 11:55:12 +0100

    Update check_site.sh

--- check_site.sh
+++ check_site.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-#Check if my site is down if so add the bad guy IP to my iptables and e-mail me
+#Verificar um HTTPS
 #Felipe Ferreira set 2013
 #rev. 08/2016 - add better output and sed to change , to . on timeto
-#rev. 02/2017 - adapted for monitoring my local site running lighttpd 
-
-#even know I use modeavsive to block more then X concurrent connections from one IP my site keeps going down!
 
 URL=$1
 KEY=$2
@@ -15,8 +12,8 @@ https_proxy=""
 LOG="/var/log/lighttpd/check_badguy.log"
 TMPN="/tmp/ntstat.tmp"
 MAXCON=20
-
-#DEBUG=
+MAXTIMEOUT=10
+#DEBUG=0
 
 if [ -z $CRIT ]; then
    echo "Usage $0 <URL> <KEYWORD> <TIMEOUT> <noproxy>"
@@ -27,24 +24,33 @@ fi
 function down() {
   date  |tee -a $LOG
   echo "CRITICAL - Site took to longer then $CRIT to respond, $MSGOK"  |tee -a $LOG
-  service lighttpd restart  |tee -a $LOG
-  netstat -ntap > $TMPN
-  CT=$(grep -c WAIT $TMPN)
+  netstat -nta |grep WAIT |grep ":80"  |grep -v "127.0.0.1" |awk '{ print $5 }' > $TMPN
+  echo "NETSTAT RESULT $TMPN"
+  CT=$(wc -l $TMPN)
   echo "WAIT CONNECTIONS: $CT" |tee -a $LOG
-  if [  "$CT" -gt "$MAXCON" ]; then
+  if [  "$CT"  ]; then
 #get IP and put in iptables rule to block it then restart apache and network
 #another way is to get "turned away. Too many connections." on /var/log/lighttpd/error.log
-  IP=$(cat $TMPN |grep WAIT |grep ":80" |grep -v "127.0.0.1" |awk '{ print $5}' |awk -F":" '{print $1}' |sort |uniq -c |sort -rn |head -n 1 |awk '{print $NF}')
-  echo "Blocking IP $IP" |tee -a $LOG
-  sed -i "9i -A INPUT -s ${IP}/32 -p tcp --dport 80 -j DROP" /etc/sysconfig/iptables
-  iptables-restore -c < /etc/sysconfig/iptables
-  iptables -L |tee -a $LOG
- else
-  echo "Could not get an offend IP" |tee -a $LOG
- fi
+  IP=$(cat $TMPN |awk -F":" '{ print $1}' |sort |uniq -c |sort -rn |head -n 1 |awk '{print $NF}')
+  echo "IP $IP"
+  if [ $IP ]; then
+   echo "Blocking IP $IP" |tee -a $LOG
+   /bin/cp -fv /etc/sysconfig/iptables /etc/sysconfig/iptables.bkp
+   iptables-save > /etc/sysconfig/iptables
+   sed -i "9i -A INPUT -s ${IP}/32 -p tcp --dport 80 -j DROP" /etc/sysconfig/iptables
+   iptables-restore -c < /etc/sysconfig/iptables
+   iptables -L |tee -a $LOG
+   service lighttpd restart  |tee -a $LOG
 #Send an email
-echo "SITE DOWN felipeferreira.net bad IP $IP" |/bin/mail -s "Site restarted" -a $LOG -r "felipe@mail.yourdomain.com"  youremail@gmail.com
+   echo "SITE DOWN felipeferreira.net bad IP $IP" |/bin/mail -s "Site restarted" -a $LOG -r "felipe@mail.net"  fel@mail.com
 
+
+  else
+  echo "Could not get an offend IP" |tee -a $LOG
+  fi
+ else
+  echo "Not maxcon $MAXCON"|tee -a $LOG
+ fi
  exit 2
 }
 
@@ -54,19 +60,19 @@ R=$(echo $((1 + RANDOM % 1000)))
 TMP="/tmp/check_http_sh_${R}_${TC}.tmp"
 touch $TMP
 
-CMD_TIME="curl --noproxy '*' -k --location --no-buffer --silent --output ${TMP} -w %{time_connect}:%{time_starttransfer}:%{time_total} '${URL}'"
+CMD_TIME="curl -m $MAXTIMEOUT -k --location --no-buffer --silent --output ${TMP} -w %{time_connect}:%{time_starttransfer}:%{time_total} '${URL}'"
 
 #echo $CMD_TIME
-TIME=`eval $CMD_TIME`
-
+TIME=$(eval $CMD_TIME)
+#echo "Done CMD"
 if [ -f $TMP ]; then
    RESULT=`grep -c $KEY $TMP`
+   TIMETOT=`echo $TIME | gawk  -F: '{ print \$3 }' |sed 's/,/./g'`
 else
-   echo "UNKOWN - Could not create tmp file $TMP"
-   exit 3
+   echo "ERROR - Could not create tmp file $TMP" |tee -a $LOG
+   echo $TIME |tee -a $LOG
+   down
 fi
-
-TIMETOT=`echo $TIME | gawk  -F: '{ print \$3 }' |sed 's/,/./g'`
 
 if [ ! -z $DEBUG ]; then
 echo "CMD_TIME: $CMD_TIME"
